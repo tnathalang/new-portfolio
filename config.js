3cf5fb91f9980ad0ee0ce6d420a342d4ca5368a1
Tom Na Thalang <nathalang_t@hotmail.ca>
2019-06-08 14:34:43 -0400

    initial commit

---
+++ config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  siteTitle: 'Gatsby Starter Spectral', // <title>
+  manifestName: 'Spectral',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Anubhav Srivastava',
+  subHeading: 'Full time Web Developer. Part time Open source contributor  ',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/anubhavsrivastava',
+    },
+    {
+      icon: 'fa-instagram',
+      name: 'Instagram',
+      url: 'https://www.instagram.com/itsdrunksushi/',
+    },
+    {
+      icon: 'fa-linkedin',
+      name: 'Facebook',
+      url: 'https://facebook.com/nt.akira',
+    },
+  ],
+};
